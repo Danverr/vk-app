f270fb04fb66cd67e34243f5f0536e35f97a8f88
Astronomax <53699562+Astronomax@users.noreply.github.com>
2020-05-05 11:26:45 +0300

    fixed folder structure

--- src/panels/calendar/calendarField/CalendarField.js
+++ src/panels/calendar/calendarField/CalendarField.js
@@ -3,7 +3,7 @@ import '@vkontakte/vkui/dist/vkui.css';
 import Icon24MoreHorizontal from '@vkontakte/icons/dist/24/more_horizontal';
 import { PanelHeaderButton, PanelHeaderBack, Panel, PanelHeader, Button, Group, Cell, Div, Avatar, CellButton, ConfigProvider, View } from '@vkontakte/vkui';
 import vkBridge from '@vkontakte/vk-bridge';
-import CircleButton from '../circleButton/CircleButton';
+import CircleButton from './circleButton/CircleButton';
 
 class CalendarField extends React.Component {
 	render() {
