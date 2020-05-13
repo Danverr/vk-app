61cf796a8aaac7eff43c0ca9cc23424e28d4baac
Arrias <43512408+Arrias@users.noreply.github.com>
2020-05-13 22:46:35 +0300

    Arrias First Commit
    Добавил компоненты поста

---
+++ src/panels/feed/Data.js
@@ -0,0 +1,9 @@
+
+
+export let Date = {
+  month: "May",
+  day: 9,
+  year: 10,
+}
+
+export default Date;
--- src/panels/feed/feed.js
+++ src/panels/feed/feed.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import {Panel, PanelHeader, View} from '@vkontakte/vkui';
-import '@vkontakte/vkui/dist/vkui.css';
 
-const Feed = (props) => {
-    return (
-        <View id={props.id}
-              activePanel={props.nav.panel}
-              history={props.nav.history}
-              onSwipeBack={props.nav.goBack}
-        >
-            <Panel id="main">
-                <PanelHeader separator={false}>Лента</PanelHeader>
-            </Panel>
-        </View>
-    );
+import React from 'react';
+import {Panel, PanelHeader, Group } from '@vkontakte/vkui';
+import s from './Feed.module.css'
+import TextPost from './components/TextPost/TextPost.js';
+
+import ava from './ava.jpg';
+import ava_danya from './danya.jpg'
+
+const user = {
+    photo_200: ava,
+    first_name: 'Albert',
+    last_name: 'Skalt'
 };
 
-export default Feed;
+const user2 = {
+  photo_200: ava_danya,
+  first_name: 'Даниил',
+  last_name: 'Маряхин'
+};
 
+const Feed = ({ id, fetchedUser }) => {
+  return (
+    <Panel id='Feed'>
+      <PanelHeader separator={false}>
+        Лента
+        </PanelHeader>
+      <Group className={s.content}>
+        <TextPost user={user} text='Я скажу то, что для тебя не новость: мир не такой уж солнечный и приветливый. Это очень опасное, жесткое место, и если только дашь слабину, он опрокинет с такой силой тебя, что больше уже не встанешь. Ни ты, ни я, никто на свете, не бьёт так сильно, как жизнь! Совсем не важно, как ты ударишь, а важно, какой держишь удар, как двигаешься вперёд. Будешь идти — ИДИ! Если с испугу не свернёшь... Только так побеждают! Если знаешь, чего ты стоишь — иди и бери своё! Но будь готов удары держать, а не забрасывай кф на 2 месяца ' description='обычный день' />
+
+        <TextPost user={user2} text='Годно! ' description='солнечно' />
+      </Group>
+    </Panel>
+  );
+}
+
+export default Feed;
