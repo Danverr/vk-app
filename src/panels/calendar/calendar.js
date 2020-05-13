b7485942635c4a30dadadec3e3d1fd1d8f9b8ba2
Astronomax <53699562+Astronomax@users.noreply.github.com>
2020-05-13 19:54:29 +0300

    react-calendar copied into project

--- src/panels/calendar/calendar.js
+++ src/panels/calendar/calendar.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import { Panel, PanelHeader, View } from '@vkontakte/vkui';
+import { Panel, PanelHeader, View, Div } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
-import { ResponsiveCalendar } from '@nivo/calendar'
-import Calendar from 'react-calendar';
-import './Calendar.css';
+import Calendar from './react-calendar';
+import './calendar.css';
 
 const CalendarPanel = (props) => {
     const [activePanel, setPanel] = useState("main");
@@ -12,7 +11,9 @@ const CalendarPanel = (props) => {
         <View id={props.id} activePanel={activePanel}>
             <Panel id="main">
                 <PanelHeader separator={false}>Календарь</PanelHeader>
+                <Div style = {{paddingTop: "0px"}}> 
                 <Calendar/>
+                </ Div>
             </Panel>
         </View>
     );
