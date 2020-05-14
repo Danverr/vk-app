3bb7e21c920cab12c3b689a9ead4b1563e575210
Astronomax <53699562+Astronomax@users.noreply.github.com>
2020-05-14 13:01:10 +0300

    first post added

--- src/panels/calendar/calendar.js
+++ src/panels/calendar/calendar.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import {Panel, PanelHeader, View, Div} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
+import TextPost from '../../components/TextPost/TextPost'
 import Calendar from './react-calendar';
 import './calendar.css';
+
+import ava from '../../img/ava.jpg';
+
+const user = {
+    photo_200: ava,
+    first_name: 'Albert',
+    last_name: 'Skalt'
+};
 
 const CalendarPanel = (props) => {
     return (
@@ -15,6 +24,7 @@ const CalendarPanel = (props) => {
                 <PanelHeader separator={false}>Календарь</PanelHeader>
                 <Div style = {{paddingTop: "0px"}}> 
                 <Calendar/>
+                <TextPost user={user} text='Я скажу то, что для тебя не новость: мир не такой уж солнечный и приветливый. Это очень опасное, жесткое место, и если только дашь слабину, он опрокинет с такой силой тебя, что больше уже не встанешь. Ни ты, ни я, никто на свете, не бьёт так сильно, как жизнь! Совсем не важно, как ты ударишь, а важно, какой держишь удар, как двигаешься вперёд. Будешь идти — ИДИ! Если с испугу не свернёшь... Только так побеждают! Если знаешь, чего ты стоишь — иди и бери своё! Но будь готов удары держать, а не забрасывай кф на 2 месяца ' description='обычный день' />
                 </ Div>
             </Panel>
         </View>
