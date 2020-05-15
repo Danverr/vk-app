a002460605068d993be12c8875f1237ae404db61
Astronomax <53699562+Astronomax@users.noreply.github.com>
2020-05-15 15:55:17 +0300

    load post from picked date

--- src/panels/calendar/calendar.js
+++ src/panels/calendar/calendar.js
@@ -4,6 +4,7 @@ import '@vkontakte/vkui/dist/vkui.css';
 import api from '../../utils/api';
 import TextPost from '../../components/TextPost/TextPost'
 import Calendar from './react-calendar';
+import { getDate, getMonth, getYear } from '@wojtekmaj/date-utils';
 import './calendar.css';
 
 import ava from '../../img/ava.jpg';
@@ -15,38 +16,26 @@ const user = {
 };
 let date = {
     day: "15",
-    month: "5",
-    hour: "1",
+    month: "05",
+    hour: "23",
     minute: "12"
 };
 
 const CalendarPanel = (props) => {
     let [usersInfo, setUsersInfo] = useState(null);
-    let [PostInfo, setPostInfo] = useState(null);
+    let [curDate, setCurDate] = useState(null);
 
-    const sendPost = async () => {
-        let data = {
-            userId: 281105343,
-            mood: 1,
-            stress: 1,
-            anxiety: 1,
-            isPublic: 1,
-            title: "MYFIRSTPOST",
-            note: "Я скажу то, что для тебя не новость: мир не такой уж солнечный и приветливый. Это очень опасное, жесткое место, и если только дашь слабину, он опрокинет с такой силой тебя, что больше уже не встанешь. Ни ты, ни я, никто на свете, не бьёт так сильно, как жизнь! Совсем не важно, как ты ударишь, а важно, какой держишь удар, как двигаешься вперёд. Будешь идти — ИДИ! Если с испугу не свернёшь... Только так побеждают! Если знаешь, чего ты стоишь — иди и бери своё! Но будь готов удары держать, а не забрасывай кф на 2 месяца"
-        };
-        const Promise = await api("POST", "/entries/", data);
-        setPostInfo(Promise.data);
-    }
     const fetchUsersInfo = async () => {
-        let data = {
-            userId: 281105343,
-            date: "2020-05-15"
-        };
-        const Promise = await api("GET", "/entries/", data);
+        let year = getYear(curDate);
+        let month = (parseInt(getMonth(curDate)) + 1).toString();
+        let day = getDate(curDate);
+       
+        const Promise = await api("GET", "/entries/", {userId: 281105343, date: year + "-" + month + "-" + day });
         setUsersInfo(Promise.data);
     }
-    //sendPost();
-    fetchUsersInfo();
+    if(curDate != null)
+        fetchUsersInfo();
+ 
     return (
         <View id={props.id}
             activePanel={props.nav.panel}
@@ -56,10 +45,9 @@ const CalendarPanel = (props) => {
             <Panel id="main">
                 <PanelHeader separator={false}>Календарь</PanelHeader>
                 <Div style={{ paddingTop: "0px" }}>
-                    <Calendar />
+                    <Calendar onClickDay = {(value, event) => setCurDate(value)}/>
                 </ Div>
-
-                {(usersInfo != null) ? (usersInfo.map((n, i) => (<TextPost user={user} text={n.note} description={n.title} date = {date} />))) : (<div />)}
+                {(usersInfo != null) ? (usersInfo.map((n, i) => (<TextPost user={user} text={n.note} description={n.title} date = {{day: "15", month: "05",hour: "23",minute: "12"}} />))) : null}
             </Panel>
         </View>
     );
