52593f827b5a0dff87a9cf82b25a47b86f66ae87
Astronomax <53699562+Astronomax@users.noreply.github.com>
2020-05-15 17:08:36 +0300

    correct date and time in posts

--- src/panels/calendar/calendar.js
+++ src/panels/calendar/calendar.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Panel, PanelHeader, View, Div } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import api from '../../utils/api';
 import TextPost from '../../components/TextPost/TextPost'
 import Calendar from './react-calendar';
-import { getDate, getMonth, getYear } from '@wojtekmaj/date-utils';
+import { getDate, getMonth, getYear, getHours, getMinutes } from '@wojtekmaj/date-utils';
 import './calendar.css';
 
 import ava from '../../img/ava.jpg';
@@ -14,28 +14,28 @@ const user = {
     first_name: 'Albert',
     last_name: 'Skalt'
 };
-let date = {
-    day: "15",
-    month: "05",
-    hour: "23",
-    minute: "12"
-};
 
 const CalendarPanel = (props) => {
     let [usersInfo, setUsersInfo] = useState(null);
     let [curDate, setCurDate] = useState(null);
 
-    const fetchUsersInfo = async () => {
-        let year = getYear(curDate);
-        let month = (parseInt(getMonth(curDate)) + 1).toString();
-        let day = getDate(curDate);
-       
-        const Promise = await api("GET", "/entries/", {userId: 281105343, date: year + "-" + month + "-" + day });
-        setUsersInfo(Promise.data);
-    }
-    if(curDate != null)
+    useEffect(() => {
+        if(curDate === null) return;
+        
+        const fetchUsersInfo = async () => {
+            let year = getYear(curDate);
+            let month = (parseInt(getMonth(curDate)) + 1).toString();
+            let day = getDate(curDate);
+           
+            const Promise = await api("GET", "/entries/", {userId: 281105343, date: year + "-" + month + "-" + day });
+            setUsersInfo(Promise.data);
+        }
+
         fetchUsersInfo();
- 
+    },
+    [curDate]
+    );
+
     return (
         <View id={props.id}
             activePanel={props.nav.panel}
@@ -47,7 +47,7 @@ const CalendarPanel = (props) => {
                 <Div style={{ paddingTop: "0px" }}>
                     <Calendar onClickDay = {(value, event) => setCurDate(value)}/>
                 </ Div>
-                {(usersInfo != null) ? (usersInfo.map((n, i) => (<TextPost user={user} text={n.note} description={n.title} date = {{day: "15", month: "05",hour: "23",minute: "12"}} />))) : null}
+                {(usersInfo != null) ? (usersInfo.map((n, i) => (<TextPost user={user} text={n.note} description={n.title} date = {{day: getDate(new Date(n.date)), month: getMonth(new Date(n.date)), hour: getHours(new Date(n.date)), minute: getMinutes(new Date(n.date))}} />))) : null}
             </Panel>
         </View>
     );
