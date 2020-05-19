2ebc71fbd266481c4949c10615e5885c4aee0801
Danverr <79198736000@mail.ru>
2020-05-20 01:58:38 +0300

    Рефакторинг навигации / Изменения структуры файлов
    - Навигация теперь в хуке useNav
    - Загрузка данных о пользователях вынесена в хук useUsersUnfo
    - Добавлена папка assets вместо img. Теперь все картинки и прочие статические штуки хранятся там

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import {Epic, Tabbar, TabbarItem, ConfigProvider} from '@vkontakte/vkui';
-import bridge from "@vkontakte/vk-bridge";
 import '@vkontakte/vkui/dist/vkui.css';
-import api from "./utils/api";
 
-import Icon28NewsfeedOutline from '@vkontakte/icons/dist/28/newsfeed_outline';
-import Icon28SmileOutline from '@vkontakte/icons/dist/28/smile_outline';
-import Icon28AddCircleOutline from '@vkontakte/icons/dist/28/add_circle_outline';
-import Icon28CalendarOutline from '@vkontakte/icons/dist/28/calendar_outline';
-import Icon28SettingsOutline from '@vkontakte/icons/dist/28/settings_outline';
+import useUsersInfo from "./utils/useUsersInfo";
+import useNav from "./utils/useNav";
 
-import Profiles from './panels/profiles/profiles';
-import Feed from "./panels/feed/feed";
-import CheckIn from "./panels/checkIn/checkIn";
-import Calendar from "./panels/calendar/calendar";
-import Settings from "./panels/settings/settings";
+import ProfilesStory from './panels/profiles/profilesStory';
+import FeedStory from "./panels/feed/feedStory";
+import CheckInStory from "./panels/checkIn/checkInStory";
+import CalendarStory from "./panels/calendar/calendarStory";
+import SettingsStory from "./panels/settings/settingsStory";
 
 // Панели по умолчанию для каждого view
 const defaultPanels = {
@@ -91,10 +86,19 @@ const App = () => {
 
         console.log(history);
     };
+import Icon28NewsfeedOutline from '@vkontakte/icons/dist/28/newsfeed_outline';
+import Icon28SmileOutline from '@vkontakte/icons/dist/28/smile_outline';
+import Icon28AddCircleOutline from '@vkontakte/icons/dist/28/add_circle_outline';
+import Icon28CalendarOutline from '@vkontakte/icons/dist/28/calendar_outline';
+import Icon28SettingsOutline from '@vkontakte/icons/dist/28/settings_outline';
+
+const App = () => {
+    const nav = useNav();
+    const usersInfo = useUsersInfo();
 
     // Добавляем обработчик события изменения истории для работы аппаратных кнопок
     useEffect(() => {
-        window.addEventListener('popstate', goBack);
+        window.addEventListener('popstate', nav.goBack);
     }, []);
 
     // Загрузка информации о пользователе и о друзьях, к которым есть доступ
@@ -137,35 +141,36 @@ const App = () => {
 
     return (
         <ConfigProvider isWebView={true}>
-            <Epic activeStory={navState.story} tabbar={
+            <Epic activeStory={nav.activeStory} tabbar={
                 <Tabbar>
                     <TabbarItem
-                        onClick={() => goTo("feed")}
-                        selected={navState.story === "feed"}
+                        onClick={() => nav.goTo("feed")}
+                        selected={nav.activeStory === "feed"}
                     ><Icon28NewsfeedOutline/></TabbarItem>
                     <TabbarItem
-                        onClick={() => goTo("profiles")}
-                        selected={navState.story === "profiles"}
+                        onClick={() => nav.goTo("profiles")}
+                        selected={nav.activeStory === "profiles"}
                     ><Icon28SmileOutline/></TabbarItem>
                     <TabbarItem
-                        onClick={() => goTo("checkIn")}
-                        selected={navState.story === "checkIn"}
+                        onClick={() => nav.goTo("checkIn")}
+                        selected={nav.activeStory === "checkIn"}
                     ><Icon28AddCircleOutline/></TabbarItem>
                     <TabbarItem
-                        onClick={() => goTo("calendar")}
-                        selected={navState.story === "calendar"}
+                        onClick={() => nav.goTo("calendar")}
+                        selected={nav.activeStory === "calendar"}
                     ><Icon28CalendarOutline/></TabbarItem>
                     <TabbarItem
-                        onClick={() => goTo("settings")}
-                        selected={navState.story === "settings"}
+                        onClick={() => nav.goTo("settings")}
+                        selected={nav.activeStory === "settings"}
                     ><Icon28SettingsOutline/></TabbarItem>
                 </Tabbar>
             }>
-                <Feed id="feed" nav={nav}/>
-                <Profiles id="profiles" nav={nav}/>
-                <CheckIn id="checkIn" nav={nav}/>
-                <Calendar id="calendar" nav={nav}/>
-                <Settings id="settings" nav={nav}/>
+                <FeedStory id="feed" nav={nav}/>
+                <ProfilesStory id="profiles" nav={nav}/>
+                <CheckInStory id="checkIn" nav={nav}/>
+                <CheckInStory id="checkIn" nav={nav}/>
+                <CalendarStory id="calendar" nav={nav}/>
+                <SettingsStory id="settings" nav={nav}/>
             </Epic>
         </ConfigProvider>
     );
