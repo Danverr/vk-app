7873386a4e85fda1512836c51e67da186a51d5ed
Astronomax <fgfgfb93@gmail.com>
2020-05-29 20:00:34 +0300

    vkui switch

--- src/panels/settings/settingsStory.js
+++ src/panels/settings/settingsStory.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Panel, PanelHeader, View} from '@vkontakte/vkui';
+import { Panel, PanelHeader, View, Cell, Switch } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 const SettingsStory = (props) => {
     return (
         <View id={props.id}
-              activePanel={props.nav.activePanel}
-              history={props.nav.viewHistory}
-              onSwipeBack={props.nav.goBack}
+            activePanel={props.nav.activePanel}
+            history={props.nav.viewHistory}
+            onSwipeBack={props.nav.goBack}
         >
             <Panel id="main">
                 <PanelHeader separator={false}>Настройки</PanelHeader>
+                <Cell asideContent={<Switch/>}>
+                    Напоминания о создании записи
+                </Cell>
             </Panel>
         </View>
     );
