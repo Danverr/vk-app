f9b312a143ea787e14627b931bf8c08bf212e52d
Astronomax <fgfgfb93@gmail.com>
2020-05-30 15:47:26 +0300

    панель Друзья

--- src/panels/settings/settingsStory.js
+++ src/panels/settings/settingsStory.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Panel, PanelHeader, View, Cell, Switch, CellButton } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
@@ -8,15 +8,17 @@ import Icon24Download from '@vkontakte/icons/dist/24/download';
 import Icon24Education from '@vkontakte/icons/dist/24/education';
 
 const SettingsStory = (props) => {
+    let[activePanel, setActivePanel] = useState(props.nav.activePanel);
+
     return (
         <View id={props.id}
-            activePanel={props.nav.activePanel}
+            activePanel={activePanel}
             history={props.nav.viewHistory}
             onSwipeBack={props.nav.goBack}
         >
             <Panel id="main">
                 <PanelHeader separator={false}>Настройки</PanelHeader>
-                <CellButton before = {<Icon24UserAdd/>}> Добавить друга </CellButton>
+                <CellButton before = {<Icon24UserAdd/>} onClick = {() => {setActivePanel('friends')}}> Добавить друга </CellButton>
                 <Cell asideContent={<Switch/>}>
                     Напоминания о создании записи
                 </Cell>
@@ -25,6 +27,9 @@ const SettingsStory = (props) => {
                 </Cell>
                 <CellButton before = {<Icon24Download/>}> Импорт записей из Daylio </CellButton>
                 <CellButton before = {<Icon24Education/>}> Пройти обучение </CellButton>
+            </Panel>
+            <Panel id = "friends">
+                <PanelHeader separator={false}>Друзья</PanelHeader>
             </Panel>
         </View>
     );
