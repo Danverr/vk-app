e3133cab6bd6a69b71f9bd93d062ccbdd7627263
Astronomax <fgfgfb93@gmail.com>
2020-06-05 15:17:50 +0300

    полностью работающая панель друзей

--- src/panels/settings/settingsStory.js
+++ src/panels/settings/settingsStory.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Panel, PanelHeader, View, Cell, Switch, CellButton, PanelHeaderBack, Avatar, Checkbox, FixedLayout, Separator, Counter, Button, Search, List, Group, Header } from '@vkontakte/vkui';
+import { Panel, PanelHeader, View, Cell, Switch, CellButton, PanelHeaderBack} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import bridge from "@vkontakte/vk-bridge";
 
@@ -8,67 +8,9 @@ import Icon24Download from '@vkontakte/icons/dist/24/download';
 import Icon24Education from '@vkontakte/icons/dist/24/education';
 
 import api from '../../utils/api'
+import FriendsPanel from './friendsPanel';
 
-import CanAddGroup from './canAdd/canAddGroup'
-import AddedGroup from './added/addedGroup'
-
-const SettingsStory = (props) => {
-    var [canAdd, setCanAdd] = useState(null);
-    var [added, setAdded] = useState(null);
-    var [waitToAdd, setWaitToAdd] = useState([]);
-
-    useEffect(() => {
-        if (!props.state.userToken) return;
-
-        const fetchUserFriends = async () => {
-            const friendsInfoPromise = await bridge.send("VKWebAppCallAPIMethod", {
-                method: "friends.get",
-                params: {
-                    access_token: props.state.userToken,
-                    v: "5.103",
-                    order: "name",
-                    fields: "photo_50, photo_100"
-                }
-            });
-
-            const addedPromise = await api("GET", "/statAccess/", {
-                fromId: props.state.userInfo.id,
-            });
-
-            const addedFriendsInfoPromise = await bridge.send("VKWebAppCallAPIMethod", {
-                method: "users.get",
-                params: {
-                    access_token: props.state.userToken,
-                    v: "5.103",
-                    user_ids: addedPromise.data.join(","),
-                    fields: "photo_50, photo_100"
-                }
-            });
-
-            let temp1 = [...friendsInfoPromise.response.items],
-                temp2 = [...addedPromise.data];
-            temp1 = temp1.filter((friend) => temp2.indexOf(friend.id) == -1);
-            temp2 = [...addedFriendsInfoPromise.response];
-
-            setCanAdd(temp1);
-            setAdded(temp2);
-        }
-        fetchUserFriends();
-    }, [props.state.userToken]);
-
-    const postEdges = async () => {
-        if (!props.state.userInfo) return;
-
-        waitToAdd.map((friend) => {
-            api("POST", "/statAccess/", {
-                fromId: props.state.userInfo.id,
-                toId: friend.id
-            });
-        })
-        setCanAdd([...canAdd.filter((friend) => waitToAdd.indexOf(friend) == -1)]);
-        setAdded([...added, ...waitToAdd]);
-    }
-    
+const SettingsStory = (props) => { 
     return (
         <View id={props.id}
             activePanel={props.nav.activePanel}
@@ -91,44 +33,7 @@ const SettingsStory = (props) => {
                 <PanelHeader separator={false} left={<PanelHeaderBack onClick={() => { props.nav.goBack(); }} />} >
                     Друзья
                 </PanelHeader>
-                <AddedGroup
-                    added={added}
-                    canAdd={canAdd}
-                    userInfo={props.state.userInfo}
-                    remove={
-                        (friend) => {
-                            let temp = [...added];
-                            temp.splice(temp.indexOf(friend), 1);
-                            setCanAdd([...canAdd, friend]);
-                            setAdded(temp);
-                        }} />
-                <CanAddGroup
-                    added={added}
-                    canAdd={canAdd}
-                    waitToAdd={waitToAdd}
-                    add={
-                        (friend) => {
-                            let temp = [...waitToAdd];
-                            temp.push(friend);
-                            setWaitToAdd(temp);
-                        }}
-                    remove={
-                        (friend) => {
-                            let temp = [...waitToAdd];
-                            if (temp.indexOf(friend) > -1)
-                                temp.splice(temp.indexOf(friend), 1);
-                            setWaitToAdd(temp);
-                        }} />
-                <FixedLayout vertical="bottom">
-                    <Separator wide />
-                    <Button size="xl" after={<Counter> {waitToAdd.length} </Counter>} onClick={() => {
-                        postEdges();
-                        setWaitToAdd([]);
-                        props.nav.goBack();
-                    }}>
-                        Сохранить
-                    </Button>
-                </FixedLayout>
+                <FriendsPanel nav = {props.nav} state = {props.state}/>
             </Panel>
         </View>
     );
